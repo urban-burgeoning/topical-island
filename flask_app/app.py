28081e6fa0df7ec909ef4e3cc5fc9d4bdd2daf01
aTYTaS <urban.burgeoning@gmail.com>
2022-06-02 21:02:56 +1000

    Built very simple homepage.

--- flask_app/app.py
+++ flask_app/app.py
@@ -2,6 +2,10 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route('/')
+def home():
+    return render_template("homepage.html")
+
 
 @app.route('/setup')
 def setup():
